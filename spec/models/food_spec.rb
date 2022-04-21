ab587584cf94d1efaa7a0a4e5e182b7d6fd6ab50
Jason Yang <jasonyangmh@gmail.com>
2022-04-21 22:18:01 +0700

    Add a spec to check validity with a valid name and price

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  it "has a valid factory" do
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:food)).to be_valid
+  end
+
+  it 'is valid with a name and a price' do
     expect(FactoryBot.build(:food)).to be_valid
   end
 end
