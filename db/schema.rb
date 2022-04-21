2866f1a34e33bdfc4abc17d6543c399836c2336a
Jason Yang <jasonyangmh@gmail.com>
2022-04-21 23:19:05 +0700

    Add description to Food model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_21_144826) do
+ActiveRecord::Schema.define(version: 2022_04_21_161710) do
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
 end
