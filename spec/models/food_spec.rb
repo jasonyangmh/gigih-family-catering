2cfa6fa890cdf28d853dd8222647e6bb6e3e5b69
Jason Yang <jasonyangmh@gmail.com>
2022-04-21 22:19:21 +0700

    Add a spec to check validity with an invalid name

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Food, type: :model do
   it 'is valid with a name and a price' do
     expect(FactoryBot.build(:food)).to be_valid
   end
+
+  it 'is invalid without a name' do
+    food = FactoryBot.build(:food, name: nil)
+    food.valid?
+    expect(food.errors[:name]).to include("can't be blank")
+  end
 end
