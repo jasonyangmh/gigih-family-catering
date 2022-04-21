6b62560f146e565b2c033b577b1c7706a95bec42
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 02:28:18 +0700

    Add a nil email validation for Customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Customer, type: :model do
   it 'is valid with an email' do
     expect(FactoryBot.build(:customer)).to be_valid
   end
+
+  it "is invalid without an email" do
+    customer = FactoryBot.build(:customer, email: nil)
+    customer.valid?
+    expect(customer.errors[:email]).to include("can't be blank")
+  end
 end
