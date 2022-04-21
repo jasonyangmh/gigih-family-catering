ea061f0000671e37d0fdf713e400cec2c27dec04
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 01:47:43 +0700

    Implement has_many Categories to Food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,5 @@ class Food < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
   validates :description, length: { maximum: 150 }
   has_many :FoodCategories
+  has_many :Categories, through: :FoodCategories
 end
