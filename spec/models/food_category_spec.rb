3abf8e1c3702eac331d682455023973e300e7168
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 01:26:45 +0700

    Add belongs_to validations to FoodCategory model

--- spec/models/food_category_spec.rb
+++ spec/models/food_category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe FoodCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without belongs_to association to Food' do
+    expect(FoodCategory.reflect_on_association(:Food).macro).to eq(:belongs_to)
+  end
+
+  it 'is invalid without belongs_to association to Category' do
+    expect(FoodCategory.reflect_on_association(:Category).macro).to eq(:belongs_to)
+  end
 end
