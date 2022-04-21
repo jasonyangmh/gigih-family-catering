4932bf35f78dcbe5896a0c50810cd270c281441a
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 00:01:27 +0700

    Generate Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_21_161710) do
+ActiveRecord::Schema.define(version: 2022_04_21_165910) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.float "price"
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
 end
