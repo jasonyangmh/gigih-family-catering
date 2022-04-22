90c91d0eae89d9ee9501e9dbd8d55b715473dbfe
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 20:26:36 +0700

    Create specs to check for food#show route

--- spec/controllers/food_controller_spec.rb
+++ spec/controllers/food_controller_spec.rb
@@ -8,12 +8,26 @@ RSpec.describe FoodController do
       get :index
       expect(assigns(:foods)).to match_array([food1, food2])
     end
-		
+
     it "renders the :index template" do
       get :index
       expect(response).to render_template :index
     end
   end
+
+	describe 'GET #show' do
+		it "assigns the requested food to @food" do
+			food = FactoryBot.create(:food)
+			get :show, params: { id: food }
+			expect(assigns(:food)).to eq food
+		end
+
+		it "renders the :show template" do
+			food = FactoryBot.create(:food)
+			get :show, params: { id: food }
+			expect(response).to render_template :show
+		end
+	end
 
   describe 'GET #new' do
 		it "assigns a new Food to @food" do
