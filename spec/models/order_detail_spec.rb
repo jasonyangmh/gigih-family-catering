2ce9566f357f8cf451c36112b918e99a427f1b86
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 11:06:51 +0700

    Add belongs_to Order test to OrderDetail spec

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe OrderDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without belongs_to association to Order' do
+    expect(OrderDetail.reflect_on_association(:Order).macro).to eq(:belongs_to)
+  end
 end
