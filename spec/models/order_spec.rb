34c6440098b985bb1e707ce9b47fe05c557b4028
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 11:09:47 +0700

    Add has_many OrderDetails test to Order spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Order, type: :model do
   it 'is invalid without belongs_to association to Customer' do
     expect(Order.reflect_on_association(:Customer).macro).to eq(:belongs_to)
   end
+
+  it 'is invalid without has_many association to OrderDetails' do
+    expect(Order.reflect_on_association(:OrderDetails).macro).to eq(:has_many)
+  end
 end
