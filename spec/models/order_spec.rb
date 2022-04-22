345e80de6dc74216b516b8846c90f111e2bb33d2
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 10:53:20 +0700

    Add belongs_to Customer validation for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without belongs_to association to Customer' do
+    expect(Order.reflect_on_association(:Customer).macro).to eq(:belongs_to)
+  end
 end
