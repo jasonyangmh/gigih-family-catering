602e821766b2a871295f07f3a0c5832c9eb8dc7f
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 22:18:46 +0700

    Comment out unused specs for Category

--- spec/views/categories/show.html.erb_spec.rb
+++ spec/views/categories/show.html.erb_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "categories/show", type: :view do
-  before(:each) do
-    @category = assign(:category, Category.create!(
-      name: "Name"
-    ))
-  end
+# RSpec.describe "categories/show", type: :view do
+#   before(:each) do
+#     @category = assign(:category, Category.create!(
+#       name: "Name"
+#     ))
+#   end
 
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Name/)
-  end
-end
+#   it "renders attributes in <p>" do
+#     render
+#     expect(rendered).to match(/Name/)
+#   end
+# end
