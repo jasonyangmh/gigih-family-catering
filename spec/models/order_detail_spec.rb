1171d0a3a6001ae74bbf97782528d89f9f690ea2
Jason Yang <jasonyangmh@gmail.com>
2022-04-22 11:07:36 +0700

    Add belongs_to Food test to OrderDetail spec

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe OrderDetail, type: :model do
   it 'is invalid without belongs_to association to Order' do
     expect(OrderDetail.reflect_on_association(:Order).macro).to eq(:belongs_to)
   end
+
+  it 'is invalid without belongs_to association to Food' do
+    expect(OrderDetail.reflect_on_association(:Food).macro).to eq(:belongs_to)
+  end
 end
