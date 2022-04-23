3472e52c88129e2352a8a5039147c999402aba46
Jason Yang <jasonyangmh@gmail.com>
2022-04-23 08:17:50 +0700

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Category.create(name: "Indonesian cuisine")
+Category.create(name: "Chinese cuisine")
+Category.create(name: "Indian cuisine")
+
+Food.create(name: "Fried Rice", price: 25000.0, description: "Just a fried rice.")
+Food.create(name: "Dumpling", price: 25000.0, description: "Just a dumpling.")
+Food.create(name: "Biryani", price: 25000.0, description: "Just a biryani.")
+
+FoodCategory.create(food_id: 1, category_id: 1)
+FoodCategory.create(food_id: 2, category_id: 2)
+FoodCategory.create(food_id: 3, category_id: 3)
