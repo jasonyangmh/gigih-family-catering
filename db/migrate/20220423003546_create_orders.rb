46ade5acb10cedfc7db04ab3692dcdf45c921839
Jason Yang <jasonyangmh@gmail.com>
2022-04-23 08:33:54 +0700

    Update default value for Order model

--- db/migrate/20220423003546_create_orders.rb
+++ db/migrate/20220423003546_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.string :email
-      t.string :status, default: "new"
+      t.string :status, default: "New"
       t.float :total_price
 
       t.timestamps
