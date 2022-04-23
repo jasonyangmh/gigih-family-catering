847bfeb61c249f0b3f29f4ffb86ec52e6a400688
Jason Yang <jasonyangmh@gmail.com>
2022-04-23 08:34:56 +0700

    Update default value to all caps for Order model

--- db/migrate/20220423003546_create_orders.rb
+++ db/migrate/20220423003546_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.string :email
-      t.string :status, default: "New"
+      t.string :status, default: "NEW"
       t.float :total_price
 
       t.timestamps
