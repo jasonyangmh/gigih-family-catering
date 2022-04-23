6882b71f6dc5c8390818d1a6b8896a18bad11eb3
Jason Yang <jasonyangmh@gmail.com>
2022-04-23 07:29:28 +0700

    Destroy Customer model

--- app/models/customer.rb
+++
@@ -1,4 +0,0 @@
-class Customer < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
-  has_many :Orders
-end
--- spec/factories/customers.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :customer do
-    email { Faker::Internet.email }
-  end
-end
